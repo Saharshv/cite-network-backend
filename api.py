c981b3213977aa65cb7a626b7b78adfb4eff50d4
Saharsh Vedi <thesaharsh@gmail.com>
2021-02-14 12:11:39 +0530

    Cleaned code

--- api.py
+++ api.py
@@ -11,22 +11,18 @@ app.config["DEBUG"] = True
 @app.route('/fact-check', methods=['POST'])
 def factCheck():
     queryParams = request.args.to_dict()
-    print(queryParams);
     newsUrl = queryParams.get('newsUrl');
     article = Article(str(newsUrl), language='en')
     article.download()
     article.parse()
     articleTitle = article.title
     articleContent = article.text
-    print(articleTitle)
     factCheckApiKey = 'YOUR_API_KEY'
     factCheckResponse = requests.get("https://factchecktools.googleapis.com/v1alpha1/claims:search", params={"query": articleTitle, "key": factCheckApiKey})
     print(factCheckResponse.content)
     if factCheckResponse.content == b'{}\n':
-        print('jgjhvjgvghjvjh')
         fakeboxResponse = requests.post("http://localhost:8080/fakebox/check", json={'url': newsUrl, 'content': articleContent, 'title': articleTitle})
         fakeboxJson = json.loads(fakeboxResponse.content.decode('utf-8'))
-        print(fakeboxJson)
         apiResponse = {"articleTitle": articleTitle, "articleBias": fakeboxJson.get('content').get('decision'), "articleCategory": fakeboxJson.get('domain').get('category')}
         return apiResponse
     return factCheckResponse.content
